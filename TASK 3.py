14e5c134923dc1730fc13299a073e9b3bbfb9534
Yassine Houllich <yassin.houllich@gmail.com>
2024-05-23 20:42:15 +0200

    PART 1: ASSIGNMENT TASKS

---
+++ TASK 3.py
@@ -0,0 +1,42 @@
+# 1. Create a tuple `coordinates` with the values (34.0522, -118.2437).
+coordinates = (34.0522, -118.2437 )
+
+# - Print each element of the tuple
+print(coordinates[0])
+print(coordinates[1])
+
+# 2. Create a dictionary `student` with the following key-value pairs:
+# - 'name': 'Alice'
+# - 'age': 24
+# - 'courses': ['Math', 'Science', 'English']
+# - Add a new key-value pair 'graduated': False.
+# - Update the 'age' value to 25.
+# - Print the dictionary.
+
+student = {
+    "name" : "Alice",
+    "age" : 24,
+    "courses" : ["Math", "Science", "English"],
+    "graduated" : "False",
+    "age" : 25
+}
+
+print(student)
+
+# 3. Create a set `unique_numbers` with the elements: {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}.
+unique_numbers = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
+print(unique_numbers)
+
+# - Add the number 6 to the set.
+unique_numbers.add(6)
+print(unique_numbers)
+
+# - Remove the number 2 from the set.
+unique_numbers.remove(2)
+print(unique_numbers)
+
+# - Check if the number 3 is in the set and print the result.
+3 in unique_numbers
+print(3 in unique_numbers)
+
+I DIDNT SEE ANY CALLS TO PRINT FOR THIS LAST EXERCISE, BUT I PUT THEM IN ANYWAYS.
